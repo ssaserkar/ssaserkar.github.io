5cc703c2b43626cbe67c227417403b06f2ec6c21
Soham Aserkar <116770046+ssaserkar@users.noreply.github.com>
2023-04-15 03:14:48 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).classList.add('fadeIn');
-  });
-});
