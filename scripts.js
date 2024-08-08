c0f6feda8641eeafeb517fa73dc63e616e1232d3
Soham Aserkar <116770046+ssaserkar@users.noreply.github.com>
2024-08-08 13:06:23 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('.nav-menu');
+function navigateToCategory(category) {
+    const categoryId = `${category}-projects`;
+    const categoryElement = document.getElementById(categoryId);
 
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-    });
-
-    document.querySelectorAll('.expand-details').forEach(link => {
-        link.addEventListener('click', event => {
-            event.preventDefault();
-            const targetId = link.getAttribute('data-target');
-            const targetElement = document.getElementById(targetId);
-            targetElement.style.display = targetElement.style.display === 'block' ? 'none' : 'block';
-        });
-    });
-});
+    if (categoryElement) {
+        categoryElement.scrollIntoView({ behavior: 'smooth' });
+    }
+}
